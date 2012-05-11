273afe1f0424d620dd3b4efb39fbea04100c7bae
Robert Link <robert.link@pnl.gov>
2012-05-11 17:32:24 -0400

    Updated test codes to use the Google GTest framework

--- test/digraph-bitvector-test.cpp
+++ test/digraph-bitvector-test.cpp
@@ -1,121 +1,170 @@
-#include <iostream>
-#include <string>
-#include <assert.h>
-#include "../digraph.hh"
 #include "../bitvector.hh"
+#include "../digraph.hh"
+#include "gtest/gtest.h"
+
+namespace {
+
 
 using std::string;
 
 typedef digraph<string> Graph;
 
 #define GET(vec,val) (vec).get(g.topological_index(val))
+#define SET(vec,val) (vec).set(g.topological_index(val))
+#define CLR(vec,val) (vec).clear(g.topological_index(val))
 
-int main(void)
-{
-  Graph g("GG");
+class digraphBitvectorTest : public ::testing::Test {
 
-  g.addedge("A","B");
-  g.addedge("A","C");
-  g.addedge("B","D");
-  g.addedge("C","X");
-  g.addedge("D","C");
-  g.addedge("D","E");
-  g.addedge("E","F");
-  g.addedge("E","G");
-  g.addedge("X","Y");
-  g.addedge("X","Z");
+protected:
+  Graph g;
 
-  g.topological_sort();
+  int N;
+  
+  digraphBitvectorTest() : g("TestGraph") {}
 
-  const int N = g.nodelist().size();
+  void SetUp() {
+    g.addedge("A","B");
+    g.addedge("A","C");
+    g.addedge("B","D");
+    g.addedge("C","X");
+    g.addedge("D","C");
+    g.addedge("D","E");
+    g.addedge("E","F");
+    g.addedge("E","G");
+    g.addedge("X","Y");
+    g.addedge("X","Z");
+
+    g.topological_sort();
+
+    N = g.nodelist().size();
+  }
+
+};
+
+TEST_F(digraphBitvectorTest, AncestorDescendant) {
   bitvector eanc(N), edesc(N);
 
-  g.find_ancestors("E",eanc);
-  g.find_descendants("E",edesc);
+  g.find_ancestors("E", eanc);
+  g.find_descendants("E", edesc);
 
-  assert( GET(eanc,"A") );
-  assert( GET(eanc,"B") );
-  assert( GET(eanc,"D") );
-  assert(!GET(eanc,"E") );
-  assert(!GET(eanc,"F") );
-  assert(!GET(eanc,"G") );
-  assert(!GET(eanc,"C") );
-  assert(!GET(eanc,"X") );
-  assert(!GET(eanc,"Y") );
-  assert(!GET(eanc,"Z") );
+  EXPECT_TRUE( GET(eanc,"A") );
+  EXPECT_TRUE( GET(eanc,"B") );
+  EXPECT_TRUE( GET(eanc,"D") );
+  EXPECT_FALSE(GET(eanc,"E") );
+  EXPECT_FALSE(GET(eanc,"F") );
+  EXPECT_FALSE(GET(eanc,"G") );
+  EXPECT_FALSE(GET(eanc,"C") );
+  EXPECT_FALSE(GET(eanc,"X") );
+  EXPECT_FALSE(GET(eanc,"Y") );
+  EXPECT_FALSE(GET(eanc,"Z") );
 
-  assert(!GET(edesc,"A") );
-  assert(!GET(edesc,"B") );
-  assert(!GET(edesc,"D") );
-  assert(!GET(edesc,"E") );
-  assert( GET(edesc,"F") );
-  assert( GET(edesc,"G") );
-  assert(!GET(edesc,"C") );
-  assert(!GET(edesc,"X") );
-  assert(!GET(edesc,"Y") );
-  assert(!GET(edesc,"Z") );
+  EXPECT_FALSE(GET(edesc,"A") );
+  EXPECT_FALSE(GET(edesc,"B") );
+  EXPECT_FALSE(GET(edesc,"D") );
+  EXPECT_FALSE(GET(edesc,"E") );
+  EXPECT_TRUE( GET(edesc,"F") );
+  EXPECT_TRUE( GET(edesc,"G") );
+  EXPECT_FALSE(GET(edesc,"C") );
+  EXPECT_FALSE(GET(edesc,"X") );
+  EXPECT_FALSE(GET(edesc,"Y") );
+  EXPECT_FALSE(GET(edesc,"Z") );
+}
 
-  // test connected component
+TEST_F(digraphBitvectorTest, ConnectedComponent) {
   bitvector ccomp(N);
   g.connected_component("E",ccomp);
 
-  assert(ccomp.count() == (unsigned)N);   // all nodes in ccomp
-  assert( GET(ccomp,"A") );
-  assert( GET(ccomp,"B") );
-  assert( GET(ccomp,"D") );
-  assert( GET(ccomp,"E") );
-  assert( GET(ccomp,"F") );
-  assert( GET(ccomp,"G") );
-  assert( GET(ccomp,"C") );
-  assert( GET(ccomp,"X") );
-  assert( GET(ccomp,"Y") );
-  assert( GET(ccomp,"Z") );
+  EXPECT_EQ(N, ccomp.count());
+  EXPECT_TRUE( GET(ccomp,"A") );
+  EXPECT_TRUE( GET(ccomp,"B") );
+  EXPECT_TRUE( GET(ccomp,"D") );
+  EXPECT_TRUE( GET(ccomp,"E") );
+  EXPECT_TRUE( GET(ccomp,"F") );
+  EXPECT_TRUE( GET(ccomp,"G") );
+  EXPECT_TRUE( GET(ccomp,"C") );
+  EXPECT_TRUE( GET(ccomp,"X") );
+  EXPECT_TRUE( GET(ccomp,"Y") );
+  EXPECT_TRUE( GET(ccomp,"Z") );
 
-  // disconnect the graph by deleting C and D.  We don't recompute the
-  // topology or make a bitvector with the new size, but all *should*
-  // go well, provided we don't refer to the deleted nodes. (Don't do
-  // this in real code.)
-  g.delnode("C");
-  g.delnode("D");
-  ccomp.clearall();
-  g.connected_component("B",ccomp);
-  assert(ccomp.count() == 2);
-  assert( GET(ccomp,"A") );
-  assert( GET(ccomp,"B") );
-  assert(!GET(ccomp,"E") );
-  assert(!GET(ccomp,"F") );
-  assert(!GET(ccomp,"G") );
-  assert(!GET(ccomp,"X") );
-  assert(!GET(ccomp,"Y") );
-  assert(!GET(ccomp,"Z") );
+  bitvector ccomp2(N), ccomp3(N);
+  g.deledge("D","E");
 
-  ccomp.clearall();
-  g.connected_component("E",ccomp);
-  assert(ccomp.count() == 3);
-  assert(!GET(ccomp,"A") );
-  assert(!GET(ccomp,"B") );
-  assert( GET(ccomp,"E") );
-  assert( GET(ccomp,"F") );
-  assert( GET(ccomp,"G") );
-  assert(!GET(ccomp,"X") );
-  assert(!GET(ccomp,"Y") );
-  assert(!GET(ccomp,"Z") );
+  g.connected_component("E",ccomp2);
+  EXPECT_EQ(3, ccomp2.count());
+  EXPECT_TRUE(  GET(ccomp2,"E") );
+  EXPECT_TRUE(  GET(ccomp2,"F") );
+  EXPECT_TRUE(  GET(ccomp2,"G") );
+  EXPECT_FALSE( GET(ccomp2,"A") );
 
-  ccomp.clearall();
-  g.connected_component("Z",ccomp);
-  assert(ccomp.count() == 3);
-  assert(!GET(ccomp,"A") );
-  assert(!GET(ccomp,"B") );
-  assert(!GET(ccomp,"E") );
-  assert(!GET(ccomp,"F") );
-  assert(!GET(ccomp,"G") );
-  assert( GET(ccomp,"X") );
-  assert( GET(ccomp,"Y") );
-  assert( GET(ccomp,"Z") );
+  g.connected_component("C",ccomp3);
+  EXPECT_EQ(7, ccomp3.count());
+  EXPECT_TRUE( GET(ccomp3,"A") );
+  EXPECT_TRUE( GET(ccomp3,"B") );
+  EXPECT_TRUE( GET(ccomp3,"D") );
+  EXPECT_TRUE( GET(ccomp3,"C") );
+  EXPECT_TRUE( GET(ccomp3,"X") );
+  EXPECT_TRUE( GET(ccomp3,"Y") );
+  EXPECT_TRUE( GET(ccomp3,"Z") );
+  EXPECT_FALSE( GET(ccomp3,"F") );
+}
 
+
+TEST_F(digraphBitvectorTest, Subgraphs) {
+  bitvector subset(N);
+  subset.setall();
+  CLR(subset,"D");                // subset includes everything but D
+
+  EXPECT_FALSE(g.is_descendant("A","G", &subset));
+  EXPECT_FALSE(g.is_ancestor("F","B", &subset));
+  EXPECT_TRUE( g.is_ancestor("Z","A", &subset));
+
+  bitvector ccompe(N),ccompx(N);
+  g.connected_component("E",ccompe, &subset);
+  EXPECT_EQ(3, ccompe.count());
+  EXPECT_TRUE(  GET(ccompe,"E") );
+  EXPECT_TRUE(  GET(ccompe,"F") );
+  EXPECT_TRUE(  GET(ccompe,"G") );
+  EXPECT_FALSE( GET(ccompe,"X") );
+  EXPECT_FALSE( GET(ccompe,"D") );  
+
+  g.connected_component("X", ccompx, &subset);
+  EXPECT_EQ(6, ccompx.count());
+  EXPECT_TRUE(  GET(ccompx,"Y") );
+  EXPECT_TRUE(  GET(ccompx,"C") );
+  EXPECT_TRUE(  GET(ccompx,"B") );
+  EXPECT_TRUE(  GET(ccompx,"A") );
+  EXPECT_FALSE( GET(ccompx,"G") );
+  EXPECT_FALSE( GET(ccompx,"D") );
+
+  bitvector bcdefg(N);
+  SET(bcdefg, "B");
+  SET(bcdefg, "C");
+  SET(bcdefg, "D");
+  SET(bcdefg, "E");
+  SET(bcdefg, "F");
+  SET(bcdefg, "G");
+
+  bitvector gsrcs(N),gsinks(N);
+
+  g.find_all_sources(bcdefg, gsrcs);
+  g.find_all_sinks(bcdefg, gsinks);
+
+  EXPECT_EQ(1, gsrcs.count());
+  EXPECT_TRUE(GET(gsrcs,"B"));
+  EXPECT_FALSE(GET(gsrcs,"C"));
+
+  EXPECT_EQ(3, gsinks.count());
+  EXPECT_TRUE(GET(gsinks,"C"));
+  EXPECT_TRUE(GET(gsinks,"F"));
+  EXPECT_TRUE(GET(gsinks,"G"));
   
-  
-  std::cout << "All asserts passed.\n";
+}
 
-  return 0;
+
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
---
+++ test/old/bitvector-test-old.cpp
@@ -0,0 +1,153 @@
+#include "../bitvector.hh"
+#include <iostream>
+#include <assert.h>
+
+int main(void)
+{
+  // test small vectors (small enough that they occupy less than 1 word)
+  bitvector even10(10),threes10(10);
+  for(int i=0;i<10;++i) {
+    if(i%2 == 0)
+      even10.set(i);
+    if(i%3 == 0)
+      threes10.set(i);
+  }
+  threes10.clear(0);
+
+  // intersection & copy constructor
+  bitvector et10int(setintersection(even10,threes10));
+  // union and operator=
+  bitvector et10union(10); 
+  et10union = threes10;
+  et10union.setunion(even10);
+  // set difference
+  bitvector et10diff(setdifference(even10,threes10)); // should get 4 results, since we removed 0 from the threes
+  
+
+  std::cout << "Small intersection:  ";
+  for(int i=0; i<10; ++i)
+    if(et10int.get(i))
+      std::cout << i << " ";
+  std::cout << "\nSmall union:  ";
+  for(int i=0; i<10; ++i)
+    if(et10union.get(i))
+      std::cout << i << " ";
+  std::cout << "\nSmall set difference:  ";
+  for(int i=0; i<10; ++i)
+    if(et10diff.get(i))
+      std::cout << i << " ";
+  std::cout << "\n" << "et10diff internal rep: " << et10diff << "\n";
+
+  // assertions
+  assert(! et10int.get(0) );
+  assert(et10union.get(0) );
+  assert( et10diff.get(0) );
+
+  assert(et10int.count() == 1);
+  assert(et10union.count() == 7);
+  assert(et10diff.count() == 4);
+
+  // test iteration
+  std::cout << "Small set difference using iteration: ";
+  bitvector_iterator bvit(&et10diff);
+  int ct=0;
+  while(bvit.next()) {
+    ct++;
+    assert(et10diff.get(bvit.bindex()));
+    std::cout << bvit.bindex() << " ";
+  }
+  std::cout << "\n";
+  assert(ct == et10diff.count());
+  
+  for(int i=1; i<10; ++i) {
+    if(i%2==0 && i%3==0)
+      assert(et10int.get(i));
+    else
+      assert(!et10int.get(i));
+
+    if(i%2==0 || i%3==0)
+      assert(et10union.get(i));
+    else
+      assert(!et10union.get(i));
+
+    if(i%2==0 && i%3!=0)
+      assert(et10diff.get(i));
+    else
+      assert(!et10diff.get(i));
+  }
+
+  // test large vectors (> 1 word)
+  bitvector even101(101), threes101(101);
+  for(int i=0;i<101;++i) {
+    if(i%2 == 0)
+      even101.set(i);
+    if(i%3 == 0)
+      threes101.set(i);
+  }
+  threes101.clear(96);
+
+  bitvector et101int(setintersection(even101,threes101));
+  bitvector et101union(101);
+  et101union = threes101;
+  et101union.setunion(even101);
+  bitvector et101diff(setdifference(even101,threes101));
+
+  std::cout << "\nLarge intersection [60,70):  ";
+  for(int i=60; i<70; ++i)
+    if(et101int.get(i))
+      std::cout << i << " ";
+  std::cout << "\nLarge union [60,70):  ";
+  for(int i=60; i<70; ++i)
+    if(et101union.get(i))
+      std::cout << i << " ";
+  std::cout << "\nLarge set difference [60,70):  ";
+  for(int i=60; i<70; ++i)
+    if(et101diff.get(i))
+      std::cout << i << " ";
+  std::cout << "\nDiff internal rep:  " << et101diff << "\n";
+
+  assert(! et101int.get(96) );
+  assert(et101union.get(96) );
+  assert( et101diff.get(96) );
+
+  int icount = 0;
+  int ucount = 1;               // for the 96 value that will get skipped below
+  int dcount = 1;               // likewise
+  
+  for(int i=0; i<101; ++i)
+    if(i != 96) {
+      if(i%2==0 && i%3==0) {
+        assert(et101int.get(i));
+        icount++;
+      }
+      if(i%2==0 || i%3==0) {
+        assert(et101union.get(i));
+        ucount++;
+      }
+      if(i%2==0 && i%3!=0) {
+        assert(et101diff.get(i));
+        dcount++;
+      }
+    }
+  
+  assert(  et101int.count() == (unsigned)icount);
+  assert(et101union.count() == (unsigned)ucount);
+  assert( et101diff.count() == (unsigned)dcount);
+
+  // add a 2^n-1 element, since these are an edge case
+  et101diff.set(31);
+
+  // test the iterator
+  bitvector_iterator lbvit(&et101diff);
+  int lct = 0;
+  while(lbvit.next()) {
+    lct++;
+    assert(et101diff.get(lbvit.bindex()));
+  }
+  std::cout << "lct: " << lct << "\tet101diff.count: " << et101diff.count();
+  assert(lct == et101diff.count());
+  
+  std::cout << "\nAll asserts passed.\n";
+
+  return 0;
+}
