d1a2aae76c12b5c717bd82917efa1878ff731098
Robert Link <robert.link@pnl.gov>
2012-05-31 09:22:43 -0400

    Added some new test graphs and a script to simplify node names in existing graphs
    The new graphs are the USA energy sector and a graph isomorphic to it but with simplified names
    
    The script simplifies names by going through the existing graph and each node it finds with a
    unique name generated from a sequence.  The sequence is a base-26 encoding with the low order
    character on the left:
    A ... Z AA BA ... ZA AB ... ZB AC ... ZZ AAA BAA ...

---
+++ data/simplify-nodenames.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+import re
+import sys
+
+def inttostring(N):
+    ## Required: N>=0
+    r = N%26
+    string = chr(ord("A")+r)
+    N /= 26
+    while(N>0):
+        r = N%26
+        string += chr(ord("A") + r-1)
+        N /= 26
+    return string
+    
+
+## this processing will simply kill any unattached nodes
+
+linematch = re.compile('([A-Za-z0-9_]+) -> ([A-Za-z0-9_]+)')
+rename_tbl = {}
+count = 0
+
+for line in sys.stdin.readlines():
+    m = linematch.search(line)
+    if(not m):
+        sys.stdout.write(line)
+    else:
+        n1 = m.group(1)
+        n2 = m.group(2)
+
+        if(not rename_tbl.has_key(n1)):
+            rename_tbl[n1] = inttostring(count)
+            count += 1
+        if(not rename_tbl.has_key(n2)):
+            rename_tbl[n2] = inttostring(count)
+            count += 1
+
+        sys.stdout.write("\t%s -> %s;\n" % (rename_tbl[n1], rename_tbl[n2]))
+
